c1e76ab5ff833c8b9c06f9533f27c942e60e380f
quincyshadow <inversenormal@gmail.com>
2019-06-21 14:04:03 -0700

    .

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if(NODE_ENV == null || NODE_ENV !== 'production') {
+if(!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
  require('dotenv').config();
 }
 const express = require("express");
